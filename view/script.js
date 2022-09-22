abb717ccb3af865b8ae5d00dfc1e3b529d515af8
twinkle-tyagi <twinkletyagi@niet.co.in>
2022-09-22 19:27:52 +0530

    task-8

---
+++ view/script.js
@@ -0,0 +1,186 @@
+/* function addToCart(event) {
+    event.preventDefault();
+
+    const btnId = event.target.className;
+    
+        //console.log(event.target.tagName);
+        //console.log(event.target.parentNode.parentNode.id);
+        //console.log(btnId);
+
+    if(event.target.tagName == 'BUTTON' && btnId == 'shop-item-button') {
+        //console.log(btnId.parentNode.id);
+        const parentId = event.target.parentNode.parentNode.id;
+        console.log(parentId);
+        console.log(btnId);
+
+        document.getElementsByClassName('cart-item')[0].innerHTML = document.getElementsByClassName('cart-item')[0].innerHTML + document.getElementById(parentId).childNodes
+
+    }
+}
+*/
+
+// if page is loaded before JS then its okay, but if page is loaded slow and loaded after JS then, JS will execute all its code and will not find any element and would give error
+// so we need to check if page is loading or done loading
+
+
+
+if(document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready);
+}
+else {
+    ready();
+}
+
+function ready () {
+    
+    var removeItems = document.getElementsByClassName('btn-danger');
+
+    for(var i =0; i<removeItems.length; i++) {    // to loop through all buttons having ID btn-danger
+        var button = removeItems[i];                    //check which one to remove
+        button.addEventListener('click', removeCartItem);   // add event listener to remove
+        console.log('clicked');
+        //var buttonClickedd = event.target.parentElement.parentElement.remove();     //remove whole div
+        //updateCartTotal();
+        //})
+    }
+    
+    var quantityInput = document.getElementsByClassName('cart-quantity-input');
+    for(var i =0; i< quantityInput.length; i++) {
+        var input = quantityInput[i];
+        input.addEventListener('change', quantityChanged);
+    }
+
+    var addToCartButton = document.getElementsByClassName('shop-item-button');
+    for(var i=0; i< addToCartButton.length; i++) {
+        var button = addToCartButton[i];
+        button.addEventListener('click', addToCartClicked);
+    }
+    document.getElementsByClassName('purchase-button')[0].addEventListener('click', purchaseClicked);
+
+//to disapper cart when we click X
+    document.getElementsByClassName('cancel')[0].addEventListener('click', () => {
+        document.getElementById('cart').style = 'display:none';
+    });
+
+    document.getElementsByClassName('cart-holder')[0].addEventListener('click', () => {
+        document.getElementById('cart').style = 'display:inline';
+    })
+}
+
+function addToCartClicked(event) {
+    var button = event.target
+    var shopItem = button.parentElement.parentElement;
+
+    var title = shopItem.getElementsByTagName('h3')[0].innerText; //here it will select only the one we want, as it is inside shopItem, so it will only access elements of shopItem.
+    console.log(title);
+    var price = shopItem.getElementsByTagName('span')[0].innerText;
+    console.log(price);
+    var imgSrc = shopItem.getElementsByTagName('img')[0].src;
+    console.log(imgSrc);
+
+    addItemToCart(title, price, imgSrc); // to add row to cart.
+    updateCartTotal();  // to update total when item added
+    showNotification(title);
+}
+
+function addItemToCart(title, price, imgSrc) {
+    var cartRow = document.createElement('div'); // create new div element
+    cartRow.classList.add('cart-row');  // to add class name
+    //cartRow.innerText = title;
+    var cartItems = document.getElementsByClassName('cart-items')[0]; // to add newly created cartRow here.
+    //cartItems.append(cartRow); //to add at end
+
+    // We have to check if we already have item in cart. If we already have item in cart, we do not add duplicate item but increase its quantity.
+    var cartItemNames = document.getElementsByClassName('cart-item-title');
+
+    for(var i =0; i< cartItemNames.length; i++) {
+        console.log(cartItemNames[i]);
+        if(cartItemNames[i].innerText == title) {   // means item already in cart
+            alert('This item is already added to cart');
+            return; // to not execute anything below
+        }
+    }
+
+    // Easy way is to copy the html code in a string and use it.
+    var cartRowContents = ` 
+    <span class='cart-item cart-column'>
+        <img class='cart-img' src="${imgSrc}" alt="" width="100" height="100">
+        <span class='cart-item-title'>${title}</span>
+    </span>
+    <span class='cart-price cart-column'>${price}</span>
+    <span class='cart-quantity cart-column'>
+        <input class='cart-quantity-input' type="number" value='1'>
+        <button class="btn btn-danger" type='button'>REMOVE</button>
+    </span> `;
+
+    cartRow.innerHTML = cartRowContents;
+    cartItems.append(cartRow);
+
+// remove button will not work as it was added after content is loaded, so we have to add event listener here to make it work
+    cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem);
+// we would do same thing with quantity as it is also added afterwards
+    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged);
+}
+
+function quantityChanged(event) {
+    var input = event.target;
+    if(isNaN(input.value) || input.value <= 0) {    //check if input items are non negative and number only.
+        input.value = 1;
+    }
+    updateCartTotal();
+}
+
+function removeCartItem(event) {
+        var buttonClickedd = event.target;
+        buttonClickedd.parentElement.parentElement.remove();     //remove whole div
+        updateCartTotal();
+}
+
+
+function updateCartTotal() {
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0];
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row');
+
+    var total = 0;
+    for(var i=0; i<cartRows.length; i++) {  // to traverse each cart rows and check and get price and quantity of clicked item
+        var cartRow = cartRows[i];
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0];
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0];
+
+        var price = parseFloat(priceElement.innerHTML.replace('$', '')); //to get text value and replace $ sign as we dont need $ sign
+        //parseFloat will convert string to float value
+        var quantity = quantityElement.value;
+        //console.log(price);
+
+        console.log(price*quantity);
+
+        total = total + (price*quantity);
+
+    }
+
+    total = Math.round(total * 100)/100; //to round our number to 2 decimal places.
+    document.getElementsByClassName('cart-total')[0].innerHTML = '$' + total;
+}
+
+function  purchaseClicked() {
+    alert('Thanks for purchase');
+// when we purchase, cart gets empty, so we will remove the parent of cart-item
+    var cartItems = document.getElementsByClassName('cart-items')[0];
+    while(cartItems.hasChildNodes()) {
+        cartItems.removeChild(cartItems.firstChild);    //loop through all the items and remove them using firstChild.
+    }
+    updateCartTotal();
+}
+
+function showNotification(title) {
+    const container = document.getElementById('ecomm');
+    const notify = document.createElement('span');
+    notify.classList.add('notify');
+    notify.innerText = `Your Item ${title} is added to cart`;
+
+    container.appendChild(notify);
+
+    setTimeout(() => {
+        notify.remove();
+    }, 3000);
+}
